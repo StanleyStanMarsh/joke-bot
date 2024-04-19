2d50ce1a36b8143ecf8e07efc851cb37fe3eed8c
StanleyStanMarsh <iastafev56@gmail.com>
2024-04-19 19:31:14 +0300

    feat: add GigaChat generated joke

---
+++ jokes/ai.py
@@ -0,0 +1,18 @@
+from gigachat import GigaChat
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+giga = GigaChat(credentials=os.getenv('GIGATOKEN'), scope='GIGACHAT_API_PERS', verify_ssl_certs=False)
+
+
+def get_joke() -> str:
+    response = giga.chat(f'Напиши несуществующий анекдот про Штирлица')
+    return response.choices[0].message.content
+
+
+def get_words_joke(words: set) -> str:
+    required_words = ', '.join(words)
+    response = giga.chat(f'Напиши анекдот про Штирлица. Обязательно используй все эти слова: {required_words}')
+    return response.choices[0].message.content
